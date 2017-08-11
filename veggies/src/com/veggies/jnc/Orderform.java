802bff60667544643b3794307ddd0f73105b929a
user <user@DESKTOP-732QH01>
2017-08-11 13:03:23 +1200

    Added functioanlity to checkbox.

--- veggies/src/com/veggies/jnc/Orderform.java
+++ veggies/src/com/veggies/jnc/Orderform.java
@@ -15,7 +15,6 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTable;
-import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 import com.mysql.jdbc.PreparedStatement;
