2cddae6eb93b8a1e509ade717b05af432f956b63
Nivya.varghese <Nivya.varghese@B204-11.soit.local>
2017-08-11 13:54:05 +1200

    HomePage Commit

--- veggies/src/com/veggies/jnc/HomePage.java
+++ veggies/src/com/veggies/jnc/HomePage.java
@@ -178,8 +178,7 @@ public class HomePage extends JFrame {
 				
                     if(chckbxPear.isSelected()){
                     	System.out.println("");
-                    	System.out.println("");
-                    	
+                                    	
                     	                    	                                      										
 					String pearSelected=chckbxPear.getText();
 					
